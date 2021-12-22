b3f91bc380337ccc6d4425c3294b053f962ec124
romulo <romulo@microdata.com.pe>
2021-12-21 20:58:21 -0500

    incluyendo php

---
+++ 17_Interactuando_con_server/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Desde un hosting</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+  <!--Carrousel-->
+  <div id="id_carrousel" class="carousel slide" data-bs-ride="carousel">
+
+    <!-- Indicators/punto -->
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#id_carrousel" data-bs-slide-to="0" class="active"></button>
+      <button type="button" data-bs-target="#id_carrousel" data-bs-slide-to="1"></button>
+      <button type="button" data-bs-target="#id_carrousel" data-bs-slide-to="2"></button>
+    </div>
+
+    <!-- The slideshow/carousel -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./images/perrito.jpg" alt="Los Angeles" class="d-block w-75">
+      </div>
+      <div class="carousel-item">
+        <img src="./images/run_run.jpg" alt="Chicago" class="d-block w-75">
+      </div>
+      <div class="carousel-item">
+        <img src="./images/baby_yoda.jpg" alt="New York" class="d-block w-75">
+      </div>
+    </div>
+
+    <!-- Left and right controls/icons -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#id_carrousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#id_carrousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </button>
+  </div>
+  <div>
+
+    <form action="accion.php" method="post">
+      <div class="mb-3 mt-3">
+        <label for="text" class="form-label">Nombre:</label>
+        <input type="text" class="form-control" id="nombre" placeholder="Ingrese su nombre" name="nombre">
+      </div>
+      <div class="mb-3">
+        <label for="number" class="form-label">Edad:</label>
+        <input type="number" class="form-control" id="edad" placeholder="Ingrese su edad" name="edad">
+      </div>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <audio controls>
+          <source src="./audio/techmo.mp3" type="audio/mpeg">
+          El navenador no soporta ese formato
+        </audio>
+      </div>
+      <div class="col-sm-6">
+        <button type="button" class="btn btn-warning" onclick="llamada()">
+          Boton
+        </button>
+      </div>
+    </div>
+  </div>
+</body>
+<script>
+  function llamada() {
+    alert('Lenguaje de lado de cliente');
+    var nombre = document.getElementById('nombre');
+    var edad = document.getElementById('edad');
+    nombre.value = 'Un nombre x';
+    edad.value = 100;
+  }
+</script>
